cb547c93a2aaaa93fad59e7fa3528fb622fd232a
HayleyHe <32903695+HayleyHe@users.noreply.github.com>
2019-01-28 00:00:18 -0500

    Add files via upload

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -2,27 +2,26 @@
 //  ViewController.swift
 //  Dicee
 //
-//  Created by dyx on 1/26/19.
-//  Copyright © 2019 dyx. All rights reserved.
+//  Created by 何谊 on 1/26/19.
+//  Copyright © 2019 何谊. All rights reserved.
 //
 
 import UIKit
 import AVFoundation
-
 class ViewController: UIViewController {
     
-    var randomDiceIndex1 :Int = 0
-    var randomDiceIndex2 :Int = 0
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
     var soundeffect: AVAudioPlayer = AVAudioPlayer()
     
     let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
-    
     @IBOutlet weak var diceImageView1: UIImageView!
+    
     @IBOutlet weak var diceImageView2: UIImageView!
-    @IBOutlet weak var diceSum: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
         
         let musicFile = Bundle.main.path(forResource: "bleep", ofType: ".wav")
         do {
@@ -33,43 +32,30 @@ class ViewController: UIViewController {
         }
         
         
-        updateDiceImages()
-        updateDiceSum()
-        
+        updateDiceImage()
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        
-        updateDiceImages()
-        updateDiceSum()
+        updateDiceImage()
         soundeffect.play()
+
     }
     
-    func updateDiceImages(){
+    func updateDiceImage(){
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
         
+        print (randomDiceIndex1)
+        
         diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
         diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
-    }
-    
-    func updateDiceSum(){
-        diceSum.text = "\(randomDiceIndex1 + randomDiceIndex2 + 2)"
+
+
     }
     
     override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
-        updateDiceImages()
+        updateDiceImage()
     }
     
-    @IBAction func showAlert() {
-        let message = "The sum of the two dices are: \(randomDiceIndex1 + randomDiceIndex2 + 2)"
-        let alert = UIAlertController(title: "Hello",
-                                      message: message,
-                                      preferredStyle: .alert)
-        let action = UIAlertAction(title: "Awesome", style: .default,
-                                   handler: nil)
-        alert.addAction(action)
-        present(alert, animated: true, completion: nil)
-    }
 }
 
